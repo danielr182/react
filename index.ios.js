721146cdb0414ccc5702074f8f141cf16d693c15
Daniel Rodríguez <daniel.rodriguez182@gmail.com>
2017-06-21 23:17:33 -0500

    se replica el códgo para ios

--- index.ios.js
+++ index.ios.js
@@ -1,35 +1,61 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
+  Button,
   View
 } from 'react-native';
+import {
+  StackNavigator,
+} from 'react-navigation';
 
-export default class InitProject extends Component {
+class HomeScreen extends Component {
+  static navigationOptions = {
+    title: 'Welcome lampara',
+  };
   render() {
+    const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
+        <Button
+          onPress={() => navigate('Chat', { user: 'Nata' })}
+          title="Chat with la nena"
+        />
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
+          To get started, edit index.android.js
         </Text>
         <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
         </Text>
       </View>
     );
   }
 }
+
+class ChatScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: `Chat with ${navigation.state.params.user}`
+  });
+  render() {
+    const { params } = this.props.navigation.state;
+    return (
+      <View>
+        <Text>Chat with la {params.user}</Text>
+      </View>
+    );
+  }
+}
+
+const InitProject = StackNavigator({
+  Home: { screen: HomeScreen },
+  Chat: { screen: ChatScreen }
+});
 
 const styles = StyleSheet.create({
   container: {
