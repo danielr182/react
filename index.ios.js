fb3bbd17a96b0a13d18f541bfec66f6ac7b5cf0a
Daniel Rodríguez <daniel.rodriguez182@gmail.com>
2017-06-23 06:30:21 -0500

    avance login form

--- index.ios.js
+++ index.ios.js
@@ -11,6 +11,8 @@ import {
   StackNavigator,
 } from 'react-navigation';
 
+import Login from './src/components/login/login';
+
 class HomeScreen extends Component {
   static navigationOptions = {
     title: 'Welcome',
@@ -26,13 +28,9 @@ class HomeScreen extends Component {
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <Login style={styles.instructions}>
+          
+        </Login>
       </View>
     );
   }
---
+++ src/components/login/login.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, Image } from 'react-native';
+
+class Login extends Component {
+    render() {
+        return (
+          <View style={styles.container}>
+            <View style={styles.logoContainer}>
+              <Image style={styles.logo} source={require('../../images/freepik.png')} />
+              <Text style={styles.title}>An app made for something great.</Text>
+            </View>
+            <View style={styles.formContainer}>
+            </View>    
+          </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#3498db'
+    },
+    logoContainer: {
+      alignItems: 'center',
+      flexGrow: 1,
+      justifyContent: 'center'
+    },
+    logo: {
+      width: 100,
+      height: 100
+    },
+    title: {
+      color: '#FFF',
+      marginTop: 1,
+      width: 120,
+      textAlign: 'center',
+      opacity: 0.9
+    }
+});
+
+export default Login;
